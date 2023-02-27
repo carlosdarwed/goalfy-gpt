3a3fc3dd0959780afbfa1da26e0f020442f9f13f
CarlosEduardoAD <karl.devcontato@gmail.com>
2023-02-27 09:28:46 -0300

    [test]

--- app_config/config.py
+++ app_config/config.py
@@ -1,13 +1,9 @@
 import os
 import sys
-sys.path.append("../.env")
 
 from pydantic import BaseSettings
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class Settings(BaseSettings):
-    app_org : str = os.getenv("ORG_KEY")
-    open_api : str = os.getenv("OPENAI_KEY")
+    app_org : str = os.environ["ORG_KEY"]
+    open_api : str = os.environ["OPENAI_KEY"]
 
